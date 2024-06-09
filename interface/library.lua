0d32755a5172925f66031bbd589a87a2b0bd253f
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-08 20:49:52 -0600

    Make library parameters a table

--- interface/library.lua
+++ interface/library.lua
@@ -1,49 +1,95 @@
 local library = {}
 
-local function close_button(name)
-	return {
-		type = "sprite-button",
-		name = name,
-		style = "close_button",
-		sprite = "utility/close_white",
-		hovered_sprite = "utility/close_black",
-		clicked_sprite = "utility/close_black",
-	}
+-- Taken from the flib test suite
+--- @param name string
+--- @param sprite string
+--- @param tooltip LocalisedString
+--- @param handler function
+local function frame_action_button(name, sprite, tooltip, handler)
+  return {
+    type = "sprite-button",
+    name = name,
+    style = "frame_action_button",
+    sprite = sprite .. "_white",
+    hovered_sprite = sprite .. "_black",
+    clicked_sprite = sprite .. "_black",
+    tooltip = tooltip,
+    handler = handler,
+  }
 end
 
+local function config_button(name, handler)
+	return frame_action_button(name, "flib_settings", { "gui.flib-settings" }, handler)
+end
+local function pin_button(name, handler)
+	return frame_action_button(name, "flib_pin", { "gui.flib-keep-open" }, handler)
+end
+local function close_button(name, handler)
+	return frame_action_button(name, "utility/close", { "gui.close-instruction" }, handler)
+end
+
+-- local function close_button(name)
+-- 	return {
+-- 		type = "sprite-button",
+-- 		name = name,
+-- 		style = "close_button",
+-- 		sprite = "utility/close_white",
+-- 		hovered_sprite = "utility/close_black",
+-- 		clicked_sprite = "utility/close_black",
+-- 	}
+-- end
+
+---@class frameWithButtonsParams
+---@field name string The name of the root frame
+---@field title LocalisedString The title of the frame
+---@field close_name string The name of the close button
+---@field close_handler fun(e) The handler of the close button
+---@field pin_name string The name of the pin button
+---@field pin_handler fun(e) The handler of the pin button
+---@field config_name string The name of the config button
+---@field config_handler fun(e) The handler of the config button
+---@field children GuiElemDef The element that is contained within the frame
 ---Creates the frame for a window with an exit button
----@param name string The name of the root frame
----@param title LocalisedString The title of the frame
----@param close_name string The name of the close button
----@param children GuiElemDef The element that is contained within the frame
+---@param params frameWithButtonsParams
 ---@return GuiElemDef
-function library.frame_with_exit(name, title, close_name, children)
+function library.frame_with_buttons(params)
+	if not params.pin_name ~= not params.pin_handler then
+		error({"library-errors.only-one-nil", {"library-errors.pin-param"}}, 2)
+	end
+	if not params.config_name ~= not params.config_handler then
+		error({"library-errors.only-one-nil", {"library-errors.config-param"}}, 2)
+	end
+	if not params.close_name ~= not params.close_handler then
+		error({"library-errors.only-one-nil", {"library-errors.close-param"}}, 2)
+	end
 	return {
-		type = "frame",
-		name = name,
+		type = "frame", name = params.name,
+		visible = false, elem_mods = { auto_center = true },
 		children = {
 			{
 				type = "flow", direction = "vertical",
 				children = {
 					{
 						type = "flow", style = "flib_titlebar_flow",
-						direction = "horizontal", drag_target = name,
+						direction = "horizontal", drag_target = params.name,
 						children = {
 							{
 								type = "label", style = "frame_title",
-								caption = title, ignored_by_interaction = true
+								caption = params.title, ignored_by_interaction = true
 							},
 							{
 								type = "empty-widget", style = "flib_titlebar_drag_handle",
 								ignored_by_interaction = true,
 							},
-							close_button(close_name)
+							params.pin_name and pin_button(params.pin_name, params.pin_handler) or {},
+							params.config_name and config_button(params.config_name, params.config_handler) or {},
+							params.close_name and close_button(params.close_name, params.close_handler) or {},
 						}
 					},
 					{
 						type = "flow", style = "inset_frame_container_horizontal_flow",
 						direction = "horizontal",
-						children = children
+						children = params.children
 					}
 				}
 			}
--- interface/main.lua
+++ interface/main.lua
@@ -1,22 +1,22 @@
 ---@type flib_gui
-local gui = require("__flib__.gui-lite")
-local gui_lib = require("__visual-gui-editor__.interface.library")
-
+local flib_gui = require("__flib__.gui-lite")
+local modules = require("__visual-gui-editor__.interface.library")
 local main = {}
 
 
 
 ---Creates the interface for the player
 ---@param player LuaPlayer
----@return LuaGuiElement
 function main.create(player)
-	local elems, root = gui.add(player.gui.screen,
-		gui_lib.frame_with_exit("visual-editor", "TEST", "close_frame", {
-			type = "label",
-			caption = "This is to test that it works :P"
-	}))
-	return root
+	local elems, root = flib_gui.add(player.gui.screen,
+		modules.frame_with_buttons{
+			name = "root-element",
+			title = "TEST",
+			children = {
+				type = "label",
+				caption = "This is to test that it works :P"
+			}
+		}
+	)
 end
-
-
 return main

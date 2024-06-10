4e268254be313f42da814a287595d320b55c83c0
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-10 17:07:19 -0600

    Put specific validation into functions

--- gui-modules/module_validation.lua
+++ gui-modules/module_validation.lua
@@ -1,11 +1,43 @@
+local function type_validation(key, value, acceptable_type, module_type)
+	local is_valid_type = false
+	for _, valid_type in pairs(acceptable_type) do
+		if type(value) == valid_type then
+			is_valid_type = true
+			break
+		end
+	end
+
+	if not is_valid_type then
+		error({"module-errors.parameter-invalid-type", module_type, key, type(value)}, 5)
+	end
+end
+
+local function enum_validation(key, value, acceptable_values, module_type)
+	if type(value) ~= "string" then return end
+	if not acceptable_values then return end
+
+	local matches = false
+	for _, valid_value in pairs(acceptable_values) do
+		if value == valid_value then
+			matches = true
+			break
+		end
+	end
+
+	if not matches then
+		error({"module-errors.parameter-invalid-value", module_type, key, value}, 5)
+	end
+end
+
 ---Validates the parameters of the module
 ---@param module GuiModuleDef
 ---@param params table
 local function validate_module_params(module, params)
-	local acceptable_description = module.parameters
+	local parameter_description = module.parameters
+	local module_type = module.module_type
 	---@type {[string]:ModuleParameterDef}
 	local missing = {} -- mark every paramtere as 'missing'
-	for key, value in pairs(acceptable_description) do
+	for key, value in pairs(parameter_description) do
 		missing[key] = value
 	end
 
@@ -13,39 +45,18 @@ local function validate_module_params(module, params)
 	for key, value in pairs(params) do
 		if key == "type" or key == "module_type" then goto continue end
 		missing[key] = nil -- mark as not missing
-		local acceptable = acceptable_description[key]
+		local acceptable = parameter_description[key]
 
 		-- Error on extra parameters
 		if not acceptable then
-			error({"module-errors.parameter-extra", module.module_type, key}, 4) -- TODO: Test all error messages
+			error({"module-errors.parameter-extra", module_type, key}, 4) -- TODO: Test all error messages
 		end
 
 		-- Error on wrong parameter type
-		local is_valid_type = false
-		for _, valid_type in pairs(acceptable.type) do
-			if type(value) == valid_type then
-				is_valid_type = true
-				break
-			end
-		end
-		if not is_valid_type then
-			error({"module-errors.parameter-invalid-type", module.module_type, key, type(value)}, 4)
-		end
+		type_validation(key, value, acceptable.type, module_type)
 
 		-- Error when a string and doesn't match an enum
-		if acceptable.enum then
-			local matches = false
-			for _, valid_value in pairs(acceptable.enum) do
-				if value == valid_value then
-					matches = true
-					break
-				end
-			end
-
-			if not matches then
-				error({"module-errors.parameter-invalid-value", module.module_type, key, value})
-			end
-		end
+		enum_validation(key, value, acceptable.enum, module_type)
 
 		-- Additional parameter checking possible?
 		-- Might grow as the parameters's fields expands

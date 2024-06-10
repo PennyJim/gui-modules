6978a91938227374360fa5d9b908ddbdbfb0bc00
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-10 16:25:01 -0600

    Write a template for modules

---
+++ gui-modules/modules/template.lua
@@ -0,0 +1,32 @@
+local module = {module_type = "my_module", handlers = {}}
+
+---@class WindowState.my_module : WindowState
+-- Where custom fields would go
+
+local handler_names = {
+	-- A generic place to make sure handler names match
+	-- in both handler definitons and in the build_func
+}
+
+---@class myModuleParams
+-- where LuaLS parameter definitons go
+---@type ModuleParameterDict
+module.parameters = {
+	-- Where gui-modules parameter definitons go
+	-- = {is_optional = false, type = {"string","table"}},
+}
+
+---Creates the frame for a window with an exit button
+---@param params myModuleParams
+---@return GuiElemDef
+function module.build_func(params)
+	return {}
+end
+
+-- How to define handlers
+-- ---@param self WindowState.window_frame
+-- module.handlers[handler_names.my_handler] = function (self)
+-- 	-- Do stuff
+-- end
+
+return module --[[@as GuiModuleDef]]

1f5848c6c6dd7ff7520c35e84c621c2a741ad10c
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-10 17:53:32 -0600

    Test split_module

--- gui-editor/data.lua
+++ gui-editor/data.lua
@@ -17,17 +17,5 @@ data:extend{
 			mipmap_count = iron_gear.icon_mipmaps,
 		},
 		toggleable = true,
-	},
-	{
-		type = "shortcut",
-		name = "visual-editor-2",
-		associated_control_input = "visual-editor",
-		action = "lua",
-		icon = {
-			filename = iron_gear.icon,
-			size = iron_gear.icon_size,
-			mipmap_count = iron_gear.icon_mipmaps,
-		},
-		toggleable = true,
 	}
 }
--- gui-editor/interface/main.lua
+++ gui-editor/interface/main.lua
@@ -1,23 +1,26 @@
-require("__gui-modules__.gui").new{
-	namespace = "testing-2", version = 0,
-	shortcut = "visual-editor-2",
-	root = "screen",
-	definition = {
-		{
-			type = "module",
-			module_type = "window_frame",
-			name = "testing-2", title = "Testing",
-			has_close_button = true,
-			children = {
-				{
-					type = "label",
-					caption = "This is to test that it works :P Alt"
-				}
+---@type GuiElemModuleDef[]
+local filler_children = {{
+	type = "label", caption = "Filler"
+}--[[@as GuiElemModuleDef]]}
+
+---@type GuiElemModuleDef[]
+local vert_pusher_children = {
+	{
+		type = "flow", direction = "vertical",
+		children = {
+			{
+				type = "empty-widget",
+				style = "flib_vertical_pusher"
+			},
+			{
+				type = "label", caption = "Filler"
+			},
+			{
+				type = "empty-widget",
+				style = "flib_vertical_pusher"
 			}
 		}
 	}
-}--[[@as GuiWindowDef]]{
-	-- other Handlers go here
 }
 
 require("__gui-modules__.gui").new{
@@ -32,9 +35,32 @@ require("__gui-modules__.gui").new{
 			has_close_button = true,
 			children = {
 				{
-					type = "label",
-					caption = "This is to test that it works :P v2"
-				}
+					type = "module", module_type = "split_pane",
+					number_of_panes = 2, direction = "horizontal",
+					frame_styles = "invisible_frame",
+					panes = {
+						{{
+							type = "module", module_type = "split_pane",
+							number_of_panes = 5, direction = "vertical",
+							panes = {
+								filler_children,
+								filler_children,
+								filler_children,
+								filler_children,
+								filler_children
+							}
+						}},
+						{{
+							type = "module", module_type = "split_pane",
+							number_of_panes = 3, direction = "vertical",
+							panes = {
+								filler_children,
+								vert_pusher_children,
+								filler_children
+							}
+						}}
+					}
+				} --[[@as SplitPaneModuleParams]]
 			}
 		}
 	}

027d26f66b6460423e379d6045c73d5e0b11f284
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-09 19:56:55 -0600

    Fix combine and repeat untils

--- gui-modules/children-iterator.lua
+++ gui-modules/children-iterator.lua
@@ -1,6 +1,14 @@
----Combine arrays
+---Puts every element of arr2 into arr1
+---@param arr1 any[]
+---@param arr2 any[]
 local function combine(arr1, arr2)
-	return table.pack(table.unpack(arr1, 1, arr1.n), table.unpack(arr2))
+	local n = arr1.n or #arr1
+	for _, v in ipairs(arr2) do
+		n = n + 1
+		arr1[n] = v
+	end
+---@diagnostic disable-next-line: inject-field
+	arr1.n = n
 end
 
 ---Returns the array of children, or an empty one if not found
@@ -33,12 +41,14 @@ end
 local function iterator(s)
 	local child
 	repeat
-		s.currentIndex, child = next(s.currentChildren, s.currentIndex)
+		repeat -- ignore non-array indexes
+			s.currentIndex, child = next(s.currentChildren, s.currentIndex)
+		until type(s.currentIndex) == "number" or type(s.currentIndex) == "nil"
 		if not s.currentIndex then
 			-- Set the current parent to the next in the list
 			repeat -- ignore non-array indexes
 				s.currentParentIndex, s.currentParent = next(s.nextParents, s.currentParentIndex)
-			until type(s.currentParentIndex) == "number"
+			until type(s.currentParentIndex) == "number" or type(s.currentParentIndex) == "nil"
 
 			if not s.currentParent then
 				return nil -- If there's no more parents, then there's no more children either
@@ -46,7 +56,7 @@ local function iterator(s)
 
 			-- Add the next children to the list
 			s.currentChildren = getChildrenArray(s.currentParent)
-			s.nextParents = combine(s.nextParents, s.currentChildren)
+			combine(s.nextParents, s.currentChildren)
 		end
 	until s.currentIndex
 	-- Return the next parent, index, and child
@@ -60,12 +70,6 @@ end
 ---@return fun(s:ChildrenIteratorState):GuiElemModuleDef[]?,integer?,GuiElemModuleDef?
 ---@return ChildrenIteratorState
 local function allChildren(element)
-	local parents
-	if element.type then
-		parents = {element, n=1}
-	else
-		parents = {element, n=#element}
-	end
 	return iterator, {
 ---@diagnostic disable-next-line: missing-fields
 		currentParent = {}, currentChildren = {},

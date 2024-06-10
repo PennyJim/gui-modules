1695f39250426c4acb6adb0a12ba4e91aaec1d5e
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-09 18:36:58 -0600

    Support the use of the array portion of the parent I do not like it, but flib supports the array of children being the parent itself. So I should as well

--- gui-modules/children-iterator.lua
+++ gui-modules/children-iterator.lua
@@ -3,55 +3,72 @@ local function combine(arr1, arr2)
 	return table.pack(table.unpack(arr1, 1, arr1.n), table.unpack(arr2))
 end
 
+---Returns the array of children, or an empty one if not found
+---@param element GuiElemModuleDef
+---@return GuiElemModuleDef[]
+local function getChildrenArray(element)
+	local children = element.children
+	if children and children[1] then
+		return children
+	elseif children then
+		return {children}
+	else
+		return element
+	end
+end
+
 ---@class ChildrenIteratorState
 ---@field currentIndex integer?
+---@field currentChildren GuiElemModuleDef[]
 ---@field currentParent GuiElemModuleDef
 ---@field currentParentIndex integer?
 ---@field nextParents GuiElemModuleDef[]
 ---
 
----Iterates over a recursive array
+---Iterates over GuiElemModuleDef, returning each child
 ---@param s ChildrenIteratorState
----@return GuiElemModuleDef? parent
----@return integer? index in the parent
+---@return GuiElemModuleDef[]? children the array the child is in
+---@return integer? index where in that array the child is
 ---@return GuiElemModuleDef? child
 local function iterator(s)
 	local child
 	repeat
-		s.currentIndex, child = next(s.currentParent.children or {}, s.currentIndex)
+		s.currentIndex, child = next(s.currentChildren, s.currentIndex)
 		if not s.currentIndex then
 			-- Set the current parent to the next in the list
-			repeat -- ignore "n" though
+			repeat -- ignore non-array indexes
 				s.currentParentIndex, s.currentParent = next(s.nextParents, s.currentParentIndex)
-			until s.currentParentIndex ~= "n"
+			until type(s.currentParentIndex) == "number"
 
 			if not s.currentParent then
-				return nil
+				return nil -- If there's no more parents, then there's no more children either
 			end
 
 			-- Add the next children to the list
-			local children = s.currentParent.children
-			if children and children[1] then
-				s.nextParents = combine(s.nextParents, children)
-			elseif children then
-				local new_length = (s.nextParents.n or #s.nextParents)+1
-				s.nextParents[new_length] = children
-	---@diagnostic disable-next-line: inject-field
-				s.nextParents.n = new_length
-			end
+			s.currentChildren = getChildrenArray(s.currentParent)
+			s.nextParents = combine(s.nextParents, s.currentChildren)
 		end
 	until s.currentIndex
 	-- Return the next parent, index, and child
-	return s.currentParent, s.currentIndex, child
+	return s.currentChildren, s.currentIndex, child
 end
 
----Returns an iterator for each child of the given element
----@param element GuiElemModuleDef
----@return fun(s:ChildrenIteratorState):GuiElemModuleDef?,integer?,GuiElemModuleDef?
+---Returns an iterator for each child of the given element.
+---The iterator returns the `children` array the child was in,
+---the `index` the child was at, and the `child` itself
+---@param element GuiElemModuleDef|GuiElemModuleDef[]
+---@return fun(s:ChildrenIteratorState):GuiElemModuleDef[]?,integer?,GuiElemModuleDef?
 ---@return ChildrenIteratorState
 local function allChildren(element)
+	local parents
+	if element.type then
+		parents = {element, n=1}
+	else
+		parents = {element, n=#element}
+	end
 	return iterator, {
-		currentParent = {},
+---@diagnostic disable-next-line: missing-fields
+		currentParent = {}, currentChildren = {},
 		nextParents = {element, n=1}
 	} --[[@as ChildrenIteratorState]]
 end

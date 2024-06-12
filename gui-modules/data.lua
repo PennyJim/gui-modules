cc7a753fa3904207af37be80e21700da5b5ce791
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-12 17:22:36 -0600

    Add which module errored to error descriptions

--- gui-modules/data.lua
+++ gui-modules/data.lua
@@ -2,25 +2,25 @@ local modules = {}
 
 ---Validates the modules and throws errors on invalid modules
 ---*now* rather than letting an someone try and use it
+---@param name string
 ---@param definition GuiModuleDef
-local function validate_module(definition)
+function validate_module.module(name, definition)
 	if modules[definition.module_type] then
-		error({"gui-errors.module-already-exists"}, 2)
+		error({"gui-errors.module-already-exists", name}, 2)
 	end
 	modules[definition.module_type] = true
 
 	local build_func = definition.build_func
 	if not build_func or type(build_func) ~= "function" then
-		error({"gui-errors.needs-build-func"}, 2)
+		error({"gui-errors.needs-build-func", name}, 2)
 	end
-	if not definition.handlers then
-		error({"gui-errors.needs-handler-table"}, 2)
+		error({"gui-errors.needs-handler-table", name}, 2)
 	end
 	local parameters = definition.parameters
 	if not parameters then
-		error({"gui-errors.needs-parameter-table"}, 2)
+		error({"gui-errors.needs-parameter-table", name}, 2)
 	end
-	modules[definition.module_type] = true
+--
 end
 
 local prefix = "gui_module_add_"
@@ -32,6 +32,6 @@ for name, setting in pairs(settings.startup) do
 		if module.module_type ~= module_name then
 			log{"gui-errors.different-expected-name", module_name, module.module_type}
 		end
-		validate_module(module)
+		validate_module.module(module_name, module)
 	end
 end

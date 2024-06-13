08d796a609265510c223ea0463b19947c4377619
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-12 19:34:51 -0600

    Start testing startup errors

---
+++ gui-modules/modules/module_validation_test.lua
@@ -0,0 +1,47 @@
+---@diagnostic disable: assign-type-mismatch
+local module = {module_type = "my_module", handlers = {} --[[@as GuiModuleEventHandlers]]}
+
+---@type ModuleParameterDict
+module.parameters = {
+	-- Where gui-modules parameter definitons go
+	my_parameter = {is_optional = false, type = {"table"}},
+	my_enum_parameter = {
+		is_optional = false, type = {"string"},
+		enum = {"one value","other value"},
+	},
+	my_optional_parameter = {
+		is_optional = true, type = {"string"},
+		default = "one value",
+	},
+	my_both_parameter = {
+		is_optional = true, type = {"string"},
+		enum = {"one value","other value"},
+		default = "one value",
+	}
+}
+
+-- Strictly speaking, this does not test every case so
+-- bugs may sneak through, but this should catch most
+
+module.module_type = "window_frame" -- Pre-existing module
+module.build_func = "not-a-function"
+module.handlers = "not-a-table"
+module.parameters = "not-a-table"
+
+module.handlers.bad_handler = "not-a-function"
+
+module.parameters[1] = "not-string-key"
+module.parameters.my_parameter = "not-a-table"
+module.parameters.my_parameter.is_optional = "not-a-boolean"
+module.parameters.my_parameter.type = "not-a-table"
+module.parameters.my_parameter.type[1] = "not-a-type"
+
+module.parameters.my_parameter.enum = {}
+module.parameters.my_enum_parameter.enum = "not-a-table"
+module.parameters.my_enum_parameter.enum[1] = 1 -- Not a string
+
+module.parameters.my_parameter.default = "extra-default"
+module.parameters.my_optional_parameter.default = {} -- not in type table
+module.parameters.my_both_parameter.default = "not-in-enum"
+
+return module --[[@as GuiModuleDef]]

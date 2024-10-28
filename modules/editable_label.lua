2df15d3c1be4601a052bd474f18477401acba2bd
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-10-28 15:59:08 -0600

    Remove custom icon picker implementation

--- modules/editable_label.lua
+++ modules/editable_label.lua
@@ -7,7 +7,6 @@ local handler_names = {
 	-- A generic place to make sure handler names match
 	-- in both handler definitons and in the build_func
 	edit_button = "editable_label.edit_button",
-	icon_button = "editable_label.icon_button",
 	confirm = "editable_label.confirm",
 	cancel = "editable_label.cancel",
 	focus = "editable_label.focus",
@@ -20,7 +19,8 @@ local handler_names = {
 ---@field default_caption LocalisedString
 ---@field confirm_handler string?
 ---@field reserve_space boolean?
----@field include_icon_picker boolean?
+---textfield values
+---@field icon_selector boolean?
 -- label values
 ---@field caption LocalisedString?
 ---@field style string?
@@ -33,7 +33,8 @@ module.parameters = {
 	default_caption = {is_optional = false, type = {"string","table"}},
 	confirm_handler = {is_optional = true, type = {"string"}},
 	reserve_space = {is_optional = true, type = {"boolean"}, default = true},
-	include_icon_picker = {is_optional = true, type = {"boolean"}, default = false},
+	-- textfield values
+	icon_selector = {is_optional = true, type = {"boolean"}, default = true},
 	-- label values
 	caption = {is_optional = true, type = {"string","table"}},
 	style = {is_optional = true, type = {"string"}},
@@ -68,7 +69,7 @@ function module.build_func(params)
 			{
 				type = "textfield", visible = false,
 				lose_focus_on_confirm = true, text = params.caption,
-				clear_and_focus_on_right_click = true,
+				icon_selector = params.icon_selector ~= false,
 				handler = {
 					[defines.events.on_gui_confirmed]=handler_names.confirm,
 					[defines.events.on_gui_closed]=handler_names.cancel,
@@ -76,26 +77,6 @@ function module.build_func(params)
 					[handler_names.unfocus]=handler_names.unfocus,
 				}
 			},
-			-- With 2.0 adding include_icon_picker
-			-- this section will be unnecessary.
-			{
-				type = "choose-elem-button", elem_type = "signal",
-				style = "choose_chat_icon_button", visible = false,
----@diagnostic disable-next-line: missing-fields
-				style_mods = {margin={1,0,0,-32}},
-				tags = {is_enabled = params.include_icon_picker or false},
-				handler = {[defines.events.on_gui_elem_changed]=handler_names.icon_button}
-			},
-			{
-				type = "sprite-button", sprite = "utility/select_icon_black",
-				visible = false, ignored_by_interaction = true,
-				style = "choose_chat_icon_button",
----@diagnostic disable-next-line: missing-fields
-				style_mods = {margin={1,0,0,-32}},
-			},
-			-- So remove it, and update the indexies in the handlers below to handle it.
-			-- The number of items switching visibility also shrinks, so
-			-- removing the open_editor function would be simple enough.
 			{
 				type = "sprite-button", style = "mini_button_aligned_to_text_vertically_when_centered",
 				tooltip = {"gui-edit-label.edit-label"}, sprite = "utility/rename_icon",
@@ -105,46 +86,21 @@ function module.build_func(params)
 	} --[[@as modules.GuiElemModuleDef]]
 end
 
----@param module LuaGuiElement
----@param open boolean
-local function open_editor(module, open)
-	local elem_picker = module.children[3]
-	module.children[1].visible = not open
-	module.children[2].visible = open
-	if elem_picker.tags.is_enabled then
-		elem_picker.visible = open
-		module.children[4].visible = open
-	end
-end
-
 ---@param state WindowState.editable_label
 module.handlers[handler_names.edit_button] = function (state, elem)
 	local module = elem.parent --[[@as LuaGuiElement]]
 	local label = module.children[1]
 	local textfield = module.children[2]
-	local button = module.children[5]
+	local button = module.children[3]
 
 	if label.visible then
-		open_editor(module, true)
+		label.visible = false
+		textfield.visible = true
 		textfield.focus()
 		button.tooltip = {"gui-edit-label.save-label"}
 		return textfield, handler_names.focus
 	else
 		return textfield, defines.events.on_gui_confirmed
-	end
-end
----@params state WindowState.editable_label
-module.handlers[handler_names.icon_button] = function (state, elem)
-	local module = elem.parent --[[@as LuaGuiElement]]
-	local textfield = module.children[2]
-	local elem_picker = module.children[3]
-
-	local text = textfield.text
-	local value = elem_picker.elem_value
-	if value then
-		-- textfield.text = text.."[img="..value.type.."."..value.name.."]"
-		textfield.text = text.."["..value.type.."="..value.name.."]"
-		elem_picker.elem_value = nil
 	end
 end
 
@@ -153,7 +109,7 @@ module.handlers[handler_names.confirm] = function (state, elem)
 	local module = elem.parent --[[@as LuaGuiElement]]
 	local label = module.children[1]
 	local textfield = module.children[2]
-	local button = module.children[5]
+	local button = module.children[3]
 
 	---@type LocalisedString
 	local new_text = textfield.text
@@ -161,7 +117,8 @@ module.handlers[handler_names.confirm] = function (state, elem)
 		new_text = label.tags.default_caption --[[@as LocalisedString]]
 	end
 	label.caption = new_text
-	open_editor(module, false)
+	label.visible = true
+	textfield.visible = false
 
 	button.tooltip = {"gui-edit-label.edit-label"}
 	state.opened = nil
@@ -176,7 +133,7 @@ module.handlers[handler_names.cancel] = function (state, elem)
 	local module = elem.parent --[[@as LuaGuiElement]]
 	local label = module.children[1]
 	local textfield = module.children[2]
-	local button = module.children[5]
+	local button = module.children[3]
 
 	local old_text = label.caption
 	if old_text == label.tags.default_caption then
@@ -184,7 +141,8 @@ module.handlers[handler_names.cancel] = function (state, elem)
 	end
 	---@cast old_text string
 	textfield.text = old_text
-	open_editor(module, false)
+	label.visible = true
+	textfield.visible = false
 	button.tooltip = {"gui-edit-label.edit-label"}
 	return nil, handler_names.unfocus
 end

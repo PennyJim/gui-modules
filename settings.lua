6d723a1e7e648f418b2a8ea0ce71b6cb6d827dff
PennyJim <40438818+PennyJim@users.noreply.github.com>
2025-01-06 11:29:51 -0700

    Actually set allowed_values

--- settings.lua
+++ settings.lua
@@ -1,15 +1,16 @@
 ---Adds the module to the list of ones considered by gui-modules
 ---@param name string
 ---@param filename string
----@return table
+---@return data.ModStringSettingPrototype
 function module_add(name, filename)
 	return {
 		type = "string-setting",
 		name = "gui_module_add_"..name,
 		setting_type = "startup",
 		default_value = filename,
+		allowed_values = {filename},
 		hidden = true,
-	}
+	} --[[@as data.ModStringSettingPrototype]]
 end
 local function main_module_add(name)
 	return module_add(name, "__gui-modules__.modules."..name)
